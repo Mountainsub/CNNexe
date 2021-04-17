9ef31fee2a8372a18d5dc7e277d0eca8403136e3
YUDAI YAMASHITA <70077254+yudaiyamashita@users.noreply.github.com>
2021-04-17 16:28:06 +0900

    Create dataextension.py

---
+++ dataextension.py
@@ -0,0 +1,29 @@
+from torchvision.datasets import CIFAR10
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import numpy as np
+import matplotlib.pyplot as plt
+
+transform = transforms.Compose([transforms.RandomAffine([-30, 30], scale=(0.8, 1.2)),  # 回転とリサイズ
+                                transforms.ToTensor()])
+cifar10_data = CIFAR10(root="./data",
+                       train=False,download=True,
+                       transform=transform)
+cifar10_classes = np.array(["airplane", "automobile", "bird", "cat", "deer",
+                            "dog", "frog", "horse", "ship", "truck"])
+print("データの数:", len(cifar10_data))
+
+n_image = 25  # 表示する画像の数
+cifar10_loader = DataLoader(cifar10_data, batch_size=n_image, shuffle=True)
+dataiter = iter(cifar10_loader)  # イテレータ
+images, labels = dataiter.next()  # 最初のバッチを取り出す
+
+plt.figure(figsize=(10,10))  # 画像の表示サイズ
+for i in range(n_image):
+    plt.subplot(5,5,i+1)
+    plt.imshow(np.transpose(images[i], (1, 2, 0)))  # チャンネルを一番後ろに
+    label = cifar10_classes[labels[i]]
+    plt.title(label)
+    plt.tick_params(labelbottom=False, labelleft=False, bottom=False, left=False)  # ラベルとメモリを非表示に
+
+plt.show()
